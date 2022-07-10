f781cc204d593acb5c0f5aff905b1d5ac58191cc
Romain <rom.jys@outlook.com>
2022-07-10 14:21:39 +0200

    install and configure Chart.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,44 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./main.css";
 import "bootswatch/dist/simplex/bootstrap.min.css";
+import Chart from "chart.js/auto";
+
+function transformProgressToChart() {
+  //Trouver tous les progress
+  const progressAll = [...document.querySelectorAll(".progress")];
+  for (const progress of progressAll) {
+    // Récupérer la valeur :
+    const ariaValue = progress.children[0].getAttribute("aria-valuenow");
+    // Ajouter canvas pour toutes les progress :
+    const canvas = document.createElement("canvas");
+    canvas.classList.add("canvasPerso");
+    progress.replaceWith(canvas);
+    drawChart(canvas, ariaValue);
+  }
+}
+
+function drawChart(canvas, arialValue) {
+  // Configuration :
+  const data = {
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [arialValue, 100 - arialValue],
+        backgroundColor: ["#d9230f", "transparent"],
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  let config = {
+    type: "doughnut",
+    data: data,
+    options: { events: [], cutout: 60 },
+  };
+  new Chart(canvas, config);
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("it works!");
+  drawChart();
+  transformProgressToChart();
 });
